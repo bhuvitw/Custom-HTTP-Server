d20daff9a713aa456f660179165077bb9f9acc0d
bhuvitw <bhuvnesh6115@gmail.com>
2026-06-25 21:50:14 +0530

    learning go, array, loops, slices, map, string, runes, byte

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"errors"
+	// "errors"
 	"fmt"
 )
 
@@ -35,12 +35,12 @@ func main() {
 	// fmt.Println(myConst)
 
 	// const pi float = 3.1451
-	var printValue string = "Hello World"
-	printMe(printValue)
+	// var printValue string = "Hello World"
+	// printMe(printValue)
 
-	var numerator int = 11
-	var denominator int = 0
-	var result, remainder, err = intDivision(numerator, denominator)
+	// var numerator int = 11
+	// var denominator int = 0
+	// var result, remainder, err = intDivision(numerator, denominator)
 	// if err != nil {
 	// 	fmt.Printf(err.Error())
 	// }else if remainder == 0{
@@ -48,37 +48,102 @@ func main() {
 	// }else {
 	// 	fmt.Printf("The result fo integer divisin is %v with remainder %v", result, remainder)
 	// }
-	switch {
-	case err != nil:
-		fmt.Printf(err.Error())
-	case remainder == 0:
-		fmt.Printf("The result fo tintefer division %v", result)
-	default:
-		fmt.Printf("The result fo integer divisin is %v with remainder %v", result, remainder)
-	}
+	// switch {
+	// case err != nil:
+	// 	fmt.Printf(err.Error())
+	// case remainder == 0:
+	// 	fmt.Printf("The result fo tintefer division %v", result)
+	// default:
+	// 	fmt.Printf("The result fo integer divisin is %v with remainder %v", result, remainder)
+	// }
 
-	switch remainder {
-	case 0:
-		fmt.Println("The divison was exact")
-	case 1, 2:
-		fmt.Println("The division was close")
-	default:
-		fmt.Println("The divison was not close")
-	}
+	// switch remainder {
+	// case 0:
+	// 	fmt.Println("The divison was exact")
+	// case 1, 2:
+	// 	fmt.Println("The division was close")
+	// default:
+	// 	fmt.Println("The divison was not close")
+	// }
 
+	// var intArr [3]int32
+	// intArr[1] = 123
+	// fmt.Println(intArr[0])
+	// fmt.Println(intArr[1:3])
+
+	// fmt.Println((&intArr[0]))
+	// fmt.Println((&intArr[1]))
+	// fmt.Println((&intArr[2]))
+
+	// var intArr [3]int32 = [3]int32{1, 2, 3}
+	// intArr := [3]int32{1, 2, 3}
+
+	// intArr := [...]int32{1, 2, 3}
+	// fmt.Println(intArr)
+
+	// var intSlice []int32 = []int32{4, 5, 6}
+	// fmt.Printf("The length is %v with capacity %v\n", len(intSlice), cap(intSlice))
+	// intSlice = append(intSlice, 7)
+	// fmt.Printf("The length is %v with capacity %v\n", len(intSlice), cap(intSlice))
+
+	// var intSlice2 []int32 = []int32{8, 9}
+	// intSlice = append(intSlice, intSlice2...)
+	// fmt.Println(intSlice)
+
+	// var intSlice3 []int32 = make(int32[], 3, 8)
+
+	// var myMap map[string]uint8 = make(map[string]uint8)
+	// fmt.Println(myMap)
+
+	// var myMap2 = map[string]uint8{"Adam": 23, "Sarah": 45}
+	// fmt.Println(myMap2)
+
+	// fmt.Println(myMap2["Adam"])
+	// var age, ok = myMap2["Jason"]
+	// // delete(myMap2, "Adam")
+	// if ok {
+	// 	fmt.Printf("The age is %v", age)
+	// } else {
+	// 	fmt.Println("Invalid Name")
+	// }
+
+	// for name := range myMap2 {
+	// 	fmt.Printf("Name: %v\n", name)
+	// }
+
+	// var i int = 0
+	// for i < 10 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// for {
+	// 	if i >= 10 {
+	// 		break
+	// 	}
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(i)
+	// }
+	var myString = "resume"
+	var indexed = myString[0]
+	fmt.Printf("%v, %T", indexed, indexed)
 }
 
-func printMe(printValue string) {
-	fmt.Println(printValue)
-}
+// func printMe(printValue string) {
+// 	fmt.Println(printValue)
+// }
 
-func intDivision(numerator int, denominator int) (int, int, error) {
-	var err error
-	if denominator == 0 {
-		err = errors.New("Cannot Divide by Zero")
-		return 0, 0, err
-	}
-	var result int = numerator / denominator
-	var remainder int = numerator % denominator
-	return result, remainder, err
-}
+// func intDivision(numerator int, denominator int) (int, int, error) {
+// 	var err error
+// 	if denominator == 0 {
+// 		err = errors.New("Cannot Divide by Zero")
+// 		return 0, 0, err
+// 	}
+// 	var result int = numerator / denominator
+// 	var remainder int = numerator % denominator
+// 	return result, remainder, err
+// }
